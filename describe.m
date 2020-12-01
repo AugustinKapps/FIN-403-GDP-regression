0906998155bd8b91dbff14243805ce7c8679e618
Lucas Giordano <giordano3102lucas@gmail.com>
2020-12-02 00:16:39 +0100

    explanatory analysis + guidelines in README

--- describe.m
+++ describe.m
@@ -1,10 +1,15 @@
-function [minimum, maximum, mean_, median_, variance_, skewness_, kurtosis_] = describe(data)
-minimum=min(data);
-maximum=max(data);
-mean_=mean(data);
-median_=median(data);
-variance_=var(data);
-skewness_=skewness(data);
-kurtosis_=kurtosis(data);
+function [matrix] = describe(table)
+data = table2array(table);
+minimum  = round(min(data),2);
+maximum  = round(max(data),2);
+mean_    = round(mean(data,'omitnan'),2);
+median_  = round(median(data,'omitnan'),2);
+variance_= round(var(data,'omitnan'),2);
+skewness_= round(skewness(data),2);
+kurtosis_= round(kurtosis(data),2);
+matrix = [minimum;maximum;mean_;median_;variance_;skewness_;kurtosis_];
+matrix = array2table(matrix);
+matrix.Properties.VariableNames = table.Properties.VariableNames;
+matrix.statistics = ["min";"max";"mean";"median";"variance";"skewness";"kurtosis"];
 end
 
--- preprocess.m
+++ preprocess.m
@@ -2,10 +2,10 @@ function [data] = preprocess(file)
 %PREPROCESS Summary of this function goes here
 %   Detailed explanation goes here
 data = readtable(file);
-start_year = 3;
-remove_col = 'COL17';
+remove_col = 'CGNP';
 data = removevars(data,{remove_col});
-data = data(start_year:end,1:end);
-data = [ones(size(data,1),1) table2array(data)]
+data = table2array(data);
+data(any(isnan(data), 2), :) = [];
+data = [ones(size(data,1),1),data];
 end
 
