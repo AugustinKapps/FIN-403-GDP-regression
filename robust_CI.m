1c3fc2e13643535a8fd3c33a366f385de6bc975c
Lucas Giordano <giordano3102lucas@gmail.com>
2020-12-26 18:18:26 +0100

    fix FGLS + robust CI + add final models notebook

--- robust_CI.m
+++ robust_CI.m
@@ -1,20 +1,20 @@
-function [CI] = robust_CI(X,model)
+function [SE, CI] = robust_CI(X,model)
 % This function uses White?s heteroscedasticity consistent estimator to 
 % produce robust heteroscedasticity estimators for beta
 %
-
-%sizes
-n=size(X,1);
-K=size(X,2);
-%confidence level
+%confidence level alpha
 alpha=0.05;
+%sizes
+n = size(X,1);
+k = size(X,2);
 %quantiles of the student distridution of degree n-K 
-lower_quantile=tinv(alpha/2,n-K);
-upper_quantile=tinv(1-alpha/2,n-K);
+lower_quantile = tinv(alpha/2,n-k);
+upper_quantile = tinv(1-alpha/2,n-k);
 % variance of beta assuming heteroscedasticity
-omega    = diag(model.residuals);
-var_beta = inv(X'*X)* X'*omega*X* inv(X'*X);
+omega    = diag(model.residuals.^2);
+tX       = inv(X'*X);
+var_beta = tX * X'* omega * X * tX;
 SE       = sqrt(diag(var_beta));
-CI=SE*[lower_quantile,upper_quantile]+model.beta;
+CI       = SE*[lower_quantile,upper_quantile]+model.beta;
 end
 
