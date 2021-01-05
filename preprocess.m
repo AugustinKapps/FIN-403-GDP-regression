881c52d2e44d990e2f665cce2509ac320fffeda7
Lucas Giordano <giordano3102lucas@gmail.com>
2021-01-05 09:42:18 +0100

    rename files + comments in notebooks 1 and 3

--- preprocess.m
+++ preprocess.m
@@ -1,13 +1,17 @@
-function [data] = preprocess(file,standardize)
-%PREPROCESS Summary of this function goes here
-%   Detailed explanation goes here
+function [data] = preprocess(file)
+%PREPROCESS Summary:
+% @param  file: (String) path to data
+% @return data: a matrix where CGNP is removed as well as all rows
+% containing missing values. 
+% /!\ note that an intercept is also added.
 data = readtable(file);
 remove_col = 'CGNP';
+% remove CGNP
 data = removevars(data,{remove_col});
 data = table2array(data);
+% filter nan rows
 data(any(isnan(data), 2), :) = [];
 data = [ones(size(data,1),1),data];
-
 
 end
 
